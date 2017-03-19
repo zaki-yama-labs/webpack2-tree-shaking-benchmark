313b875c42cc2d12b2d24c31e82ef12dfb603ff9
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-03-19 12:45:32 +0900

    webpack1.14.0

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Button } from 'react-lightning-design-system';
+
+render(
+  <Button brand>Press me</Button>,
+  document.getElementById('root')
+);
